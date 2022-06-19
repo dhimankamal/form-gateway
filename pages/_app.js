a6651911d307544280428e9db4e38016c5e58ce2
kamal <dhimank079@gmail.com>
2022-06-19 08:27:07 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }) {
+  return (
+    <>
+     <Head>
+          <title>Form</title>
+          <meta charSet='utf-8' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
+     <div>
+      <Navbar />
+      <div className='px-10 lg:px-80'>
+      <Component {...pageProps} />
+      </div>
+     <Footer />
+    </div>
+    </>
+   
+  )
 }
 
 export default MyApp
