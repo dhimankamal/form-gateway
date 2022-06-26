411ce43ad18576b3d31d5a16d10f443601155b6d
kamal <dhimank079@gmail.com>
2022-06-26 20:38:20 +0530

    fix index file

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import hero from '../images/hero.jpg';
 import rappers from '../images/rappers.png';
+import {useState ,useEffect} from 'react';
+import { useRouter } from 'next/router'
 
 
 export default function Home() {
